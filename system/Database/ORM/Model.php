766eed61c3a9aa0f357a0d5aa4c88afd071f4bf5
sobhan khorshidi <94145282+sobhanprog@users.noreply.github.com>
2023-04-28 23:51:53 +0330

    write codes Model and HasAttributes

--- system/Database/ORM/Model.php
+++ system/Database/ORM/Model.php
@@ -8,4 +8,14 @@ abstract class Model
 {
     use Traits\HasAttributes, Traits\HasCRUD, Traits\HasRelation, Traits\HasMethodCaller, Traits\HasRelation, Traits\HasQueryBuilder, Traits\HasSoftDelete;
 
+    protected $table;
+    protected $fillable = [];
+    protected $hidden = [];
+    protected $casts = [];
+    protected $primaryKey = 'id';
+    protected $createdAt = 'create_at';
+    protected $updatedAt = 'updated_at';
+    protected $deletedAt = null;
+    protected $collection = [];
+
 }
--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -4,5 +4,12 @@ namespace System\Database\Traits;
 
 trait HasAttributes
 {
-
+    private function registerAttribue(){}
+    protected function arrayToAttribute(){}
+    protected function arrayToObject(){}
+    private function inHiddenAttributes(){}
+    private function inCastsAttributes(){}
+    private function castDecodeValue(){}
+    private function castEncodeValue(){}
+    private function arrayToCastEncodeValue(){}
 }

4e6e4e8497fd3a338c44fa405cd8353d07df425e
sobhan khorshidi <94145282+sobhanprog@users.noreply.github.com>
2023-04-30 16:00:54 +0330

    defined methodes trith HsaCRUD fill(),saveMethod()

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -2,7 +2,56 @@
 
 namespace System\Database\Traits;
 
+use System\Database\DBConnction\DBConnction;
+
 trait HasCRUD
 {
+
+    protected function saveMethod()
+    {
+        $fillString = $this->fill();
+        if (!isset($this->{$this->primaryKey})) {
+            $this->setSql("INSERT INTO " . $this->getTabelName() . " SET $fillString  , " . $this->getAttributName($this->createdAt) . "= Now()");
+        } else {
+            $this->setSql("UPDATE " . $this->getTabelName() . " SET $fillString  , " . $this->getAttributName($this->updatedAt) . "= Now()");
+            $this->setWhere("AND", $this->getAttributName($this->primaryKey) . "= ?");
+            $this->addValue($this->primaryKey, $this->{$this->primaryKey});
+        }
+        $this->execueQuery();
+        $this->restQuery();
+
+        if (!isset($this->{$this->primaryKey})) {
+            $boject = $this->findMethod(DBConnction::newInsertId());
+            $defaultVars = get_class_vars(get_called_class());
+            $allVars = get_object_vars($boject);
+            $diffrentVsars = array_diff(arrray_keys($defaultVars), array_keys($allVars));
+            foreach ($diffrentVsars as $attribute) {
+                $this->inCastsAttributes($attribute) == true ?
+                    $this->registerAttribue($this, $attribute, $this->castEncodeValue($attribute, $boject->$attribute)) :
+                    $this->registerAttribue($this, $attribute, $boject->$attribute);
+            }
+
+        }
+        $this->restQuery();
+        $this->setAllowedMethod(['update', 'delete', 'find']);
+        return $this;
+
+
+    }
+
+    protected function fill()
+    {
+        $fillArray = [];
+        foreach ($this->fillable as $attribute) {
+            if (isset($this->$attribute)) {
+                array_push($fillArray, $this->getAttributeName($attribute) . " = ?");
+                $this->inCastsAttributes($attribute) == true ?
+                    $this->addValue($attribute, $this->castEncodeValue($attribute, $this->$attribute)) :
+                    $this->addValue($attribute, $this->$attribute);
+            }
+        }
+        $fillString = implode(",", $fillArray);
+        return $fillString;
+    }
 
 }
